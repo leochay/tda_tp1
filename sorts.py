bb7f5510081fbb7e7277ec9523bd47cc6ef4c6f6
Leonardo Rodriguez <leochay@MacBook-de-Leonardo.local>
2018-04-08 01:38:47 -0300

    sorts

--- sorts.py
+++ sorts.py
@@ -1,30 +1,25 @@
-#unsorted list uList
-#sorted list sList
-#do 
-#take smallest from unsorted List
-#insert in the sorted list
-#while there is element in the unsorted list
+import sys
 
-def selectionsort(array):
-	for j in xrange(len(array)):
-		iMin=j
-		for i in xrange(j+1,len(array)):
-			if array[i] <array[iMin]:
-				iMin=i
+def selectionsort(a):
+  for j in xrange(len(a)):
+    iMin=j
+    for i in xrange(j+1,len(a)):
+      if a[i] < a[iMin]:
+        iMin=i
 
-		if iMin!=j:
-			aux = array[j]
-			aarray[j]=array[iMin]
-			array[iMin]=aux
-		#	print(a[:j])
-def insertionsort(array):
-    for i in xrange(1, len(array)):
+    if iMin!=j:
+      aux = a[j]
+      a[j]=a[iMin]
+      a[iMin]=aux
+    # print(a[:j])
+def insertionsort(a):
+    for i in xrange(1, len(a)):
         j = i-1 
-        key = array[i]
-        while (array[j] > key) and (j >= 0):
-           array[j+1] = array[j]
+        key = a[i]
+        while (a[j] > key) and (j >= 0):
+           a[j+1] = a[j]
            j -= 1
-        [j+1] = key
+        a[j+1] = key
 
 def quicksort(arr):
     less = []
@@ -45,40 +40,41 @@ def quicksort(arr):
         more = quicksort(more)
         return less + pivotList + more
 
-def heapsort(array):
+def heapsort(lst):
   ''' Heapsort. Note: this function sorts in-place (it mutates the list). '''
  
   # in pseudo-code, heapify only called once, so inline it here
-  for start in range((len(array)-2)/2, -1, -1):
-    siftdown(array, start, len(array)-1)
+  for start in range((len(lst)-2)/2, -1, -1):
+    siftdown(lst, start, len(lst)-1)
  
-  for end in range(len(array)-1, 0, -1):
-    array[end], array[0] = array[0], array[end]
-    siftdown(array, 0, end - 1)
-  return array
+  for end in range(len(lst)-1, 0, -1):
+    lst[end], lst[0] = lst[0], lst[end]
+    siftdown(lst, 0, end - 1)
+  return lst
  
-def siftdown(array, start, end):
+def siftdown(lst, start, end):
   root = start
   while True:
     child = root * 2 + 1
     if child > end: break
-    if child + 1 <= end and array[child] > array[child + 1]:
+    if child + 1 <= end and lst[child] < lst[child + 1]:
       child += 1
-    if array[root] > array[child]:
-      array[root], array[child] = array[child], array[root]
+    if lst[root] < lst[child]:
+      lst[root], lst[child] = lst[child], lst[root]
       root = child
     else:
       break
 
+
 def mergesort(array):
-	if len(array) < 2:
-		return array
+  if len(array) < 2:
+    return array
 
-	middle = int(len(array)/2)
-	left = mergesort(array[:middle])
-	right = mergesort(array[middle:])
+  middle = int(len(array)/2)
+  left = mergesort(array[:middle])
+  right = mergesort(array[middle:])
 
-	return merge(left, right)
+  return merge(left, right)
 
 def merge(left, right):
     result = []
@@ -96,63 +92,201 @@ def merge(left, right):
         result.extend(left[left_idx:])
     if right_idx < len(right):
         result.extend(right[right_idx:])
-    return result            
+    return result        
 
 def runSorts(filerandom):
-	b=[]
-	with open(filerandom, 'r') as f:
-		for line in f:
-			b.append(int(line))
-	#print(b)
-	from timeit import default_timer as timer
-	a=b[:]#slice create a copy of the array
-	start = timer()
-	selectionsort(a)
-	end = timer()
-	print(end - start)
-	a=b[:]
-	start = timer()
-	insertionsort(a)
-	end = timer()
-	print(end - start)
-	a=b[:]
-	start = timer()
-	quicksort(a)
-	end = timer()
-	print(end - start)
-	a=b[:]
-	start = timer()
-	heapsort(a)
-	end = timer()
-	print(end - start)
-	a=b[:]
-	start = timer()
-	mergesort(a)
-	end = timer()
-	print(end - start)
+  b=[]
+  with open(filerandom, 'r') as f:
+    for line in f:
+      b.append(int(line))
+  #print(b)
+  from timeit import default_timer as timer
+  a=b[:]#slice create a copy of the array
+  start = timer()
+  selectionsort(a)
+  end = timer()
+  print(end - start)
+  a=b[:]
+  start = timer()
+  insertionsort(a)
+  end = timer()
+  print(end - start)
+  a=b[:]
+  start = timer()
+  quicksort(a)
+  end = timer()
+  print(end - start)
+  a=b[:]
+  start = timer()
+  heapsort(a)
+  end = timer()
+  print(end - start)
+  a=b[:]
+  start = timer()
+  mergesort(a)
+  end = timer()
+  print(end - start)
 
-	print(b)
+  print(b)
+  return
+
+def selection(filerandom):
+  print("selection")
+  b=[]
+  with open(filerandom, 'r') as f:
+    for line in f:
+      b.append(int(line))
+  #print(b)
+  from timeit import default_timer as timer
+  #slice create a copy of the array
+  results={}
+  mean=0
+  for x in xrange(0,20):
+    print(x)
+    a=b[:]
+    start = timer()
+    selectionsort(a)
+    end = timer()
+    results[x]=end-start
+    print (results[x])
+    mean+=end-start
+  print(x)
+  mean=mean/(x+1)
+  print (mean)
+  return
 
 
+def insertion(filerandom):
+  print("selection")
+  b=[]
+  with open(filerandom, 'r') as f:
+    for line in f:
+      b.append(int(line))
+  #print(b)
+  from timeit import default_timer as timer
+  #slice create a copy of the array
+  results={}
+  mean=0
+  for x in xrange(0,5):
+    print(x)
+    a=b[:]
+    start = timer()
+    insertionsort(a)
+    end = timer()
+    results[x]=end-start
+    print (results[x])
+    mean+=end-start
+  print(x)
+  mean=mean/(x+1)
+  print (mean)
+  return
+
+def insertion(filerandom):
+  print("selection")
+  b=[]
+  with open(filerandom, 'r') as f:
+    for line in f:
+      b.append(int(line))
+  #print(b)
+  from timeit import default_timer as timer
+  #slice create a copy of the array
+  results={}
+  mean=0
+  for x in xrange(0,5):
+    print(x)
+    a=b[:]
+    start = timer()
+    quicksort(a)
+    end = timer()
+    results[x]=end-start
+    print (results[x])
+    mean+=end-start
+  print(x)
+  mean=mean/(x+1)
+  print (mean)
+  return
+
+def sort(type,number,worstcase):
+
+  sorts = {'selection': selectionsort,
+  'insertion': insertionsort,
+  'quick': quicksort,
+  'merge': mergesort,
+  'heap': heapsort}
+
+  method_name = myargs['-i']
+     # set by the command line options
+    #if myargs[]
+  if type not in sorts:
+    raise Exception("Sort Algorithm %s not implemented" % type)
+    exit()
+
+  if worstcase:
+    file='worstcase'
+  else:
+    file=number+'.random'
+  b=[]
+  with open(file, 'r') as f:
+    for line in f:
+      b.append(int(line))
+  #print(b)
+  from timeit import default_timer as timer
+  #slice create a copy of the array
+  results={}
+  mean=0
+  for x in xrange(0,10):
+    print(x)
+    a=b[:]
+    start = timer()
+    sorts[type](a)
+    end = timer()
+    results[x]=end-start
+    print (results[x])
+    mean+=end-start
+  print(x)
+  mean=mean/(x+1)
+  print (mean)
+  return
+
+def runAll():
+  print ("run all")
+  return
+
+def getopts(argv):
+    opts = {}  # Empty dictionary to store key-value pairs.
+    while argv:  # While there are arguments left to parse...
+        if argv[0][0] == '-':  # Found a "-name value" pair.
+            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
+        argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
+    return opts
+
+if __name__ == '__main__':
+    from sys import argv
+    myargs = getopts(argv)
+    if myargs.has_key('-i'):
+      sort(myargs['-i'],myargs['-n'],myargs.has_key('-w'))
+    else:
+      runAll()
+    exit()
 #main
-print("selection sort")
-#unsortedList=[20,10,50,70,100,3,2,9]
-#f = open('50.random', 'r')
-#list(f)
-#two forms of running a file load
-#first form simple code but not memory efficient
-with open('50.random', 'r') as f:
-	a=list(f)
-print(a)
-#doble array conversion from string to int
-a = [int(i) for i in a]
+#print("selection sort")
+##unsortedList=[20,10,50,70,100,3,2,9]
+##f = open('50.random', 'r')
+##list(f)
+##two forms of running a file load
+##first form simple code but not memory efficient
+#with open('50.random', 'r') as f:
+# a=list(f)
 #print(a)
-#second form
-b=[]
-with open('50.random', 'r') as f:
-	for line in f:
-		b.append(int(line))
-#print(b)
+##doble array conversion from string to int
+#a = [int(i) for i in a]
+##print(a)
+##second form
+#b=[]
+#with open('50.random', 'r') as f:
+# for line in f:
+#   b.append(int(line))
+##print(b)
 
 runSorts('50.random')
 runSorts('100.random')
@@ -164,19 +298,3 @@ runSorts('4000.random')
 runSorts('5000.random')
 runSorts('7500.random')
 runSorts('10000 .random')
-
-#MIT IMPLEMENTATION
-#def selSort(L):
-#    for i in range(len(L) - 1):
-#        minIndx = i
-#        minVal = L[i]
-#        j = i+1
-#        while j < len(L):
-#            if minVal > L[j]:
-#                minIndx = j
-#                minVal = L[j]
-#            j += 1
-#        if minIndx != i:
-#            temp = L[i]
-#            L[i] = L[minIndx]
-#            L[minIndx] = temp	
